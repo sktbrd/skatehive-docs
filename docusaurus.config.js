696b5e9545e456497fc363f8c8af410261cf70a1
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-01-29 23:43:13 -0300

    some docs adds

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Welcome to the Web3 Skateboarding Worlds',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
